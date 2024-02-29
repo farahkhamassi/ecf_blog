8d9bce1e95d663430af9be40d8bb838663a9ca55
farahkhamassi <149404206+farahkhamassi@users.noreply.github.com>
2024-02-29 15:45:29 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+$host = 'localhost';
+$db   = 'ecf_blog';
+$user = 'root';
+$pass = '';
+$port = '3306';
+
+$dsn = "mysql:host=$host;port=$port;dbname=$db";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+     $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+?>
